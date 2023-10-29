bd3700ca5314061bba070711ee535b725108938f
Filip Działa <fi.dziala@gmail.com>
2023-10-29 23:38:38 +0100

    CodeWars

---
+++ src/Kata1.java
@@ -0,0 +1,21 @@
+public class Kata1 {
+    //Suma wartości tablicy
+    public static void main(String[] args) {
+        double[] array = {1.1, 2, 3};
+        System.out.println(SumArray(array));
+
+    }
+    public static double SumArray(double[] array)
+    {
+        double sum = 0;
+        if ( array.length == 0){
+            return 0;
+        }
+        else{
+            for(int i = 0; i < array.length; i++){
+                sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
